b4c820b51b8d13f41e0f1a86f6fac6d33f50d934
sentense1 <3xsaint@gmail.com>
2023-10-05 14:44:20 +0100

    task 10 and 11

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -2,7 +2,9 @@
 """ Module for type hint """
 from typing import Sequence, Any, Union
 
+
 def safe_first_element(lst: Sequence) -> Union[Any, None]:
+    """ Duck typing - first element of a sequence """
     if lst:
         return lst[0]
     else:
--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
-""" Module for type hint """
 
+""" Module for type hint """
 from typing import Mapping, Any, TypeVar, Union
+
 
 # Define a TypeVar for the return type
 T = TypeVar('T')
 
+
 def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+    """ Returns sequence of elements, but the specific types of elements are not known. """
     if key in dct:
         return dct[key]
     else:
