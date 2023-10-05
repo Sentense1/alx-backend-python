2d10cf640a4110fdcd10cc7dd2580ce037ba3f5a
sentense1 <3xsaint@gmail.com>
2023-10-05 14:11:38 +0100

    task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,7 +3,6 @@
 """ Module for defining and working with type hints """
 from typing import Union, Tuple
 
-def to_kv(k: str, v: int | float) -> Tuple[Union[str, float]]:
-    """ Taske a str and float/int and returns a tuple """
-    my_tuple: tuple = (k, float(v * v))
-    return my_tuple
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Takes a string k and an int or float v, and returns a tuple."""
+    return k, float(v * v)
