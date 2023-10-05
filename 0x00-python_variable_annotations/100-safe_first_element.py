1c05ddc7a8ef1824d7ed626c278c767f04b7e163
sentense1 <3xsaint@gmail.com>
2023-10-05 14:39:22 +0100

    task 10 and 11

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Module for type hint """
+from typing import Sequence, Any, Union
+
+def safe_first_element(lst: Sequence) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
