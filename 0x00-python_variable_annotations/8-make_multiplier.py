48396ebde9cc51fa2a4dcc67773d5b44d2a45f84
sentense1 <3xsaint@gmail.com>
2023-10-05 14:18:37 +0100

    task 8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Module for type hint """
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Takes a float multiplier and returns a function to multiply a float."""
+    def multiplier_function(x: float) -> float:
+        return x * multiplier
+    return multiplier_function
