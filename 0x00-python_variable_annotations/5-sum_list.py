5ead6d76e82d50daad13c478a6d209434ca09a22
sentense1 <3xsaint@gmail.com>
2023-10-05 14:02:40 +0100

    task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -7,3 +7,10 @@ from typing import List
 def sum_list(input_list: List[float]) -> float:
     """ Takes a list input_list of floats and returns their sum as a float. """
     return (sum(input_list))
+
+if __name__ == '__main__':
+    floats = [3.14, 1.11, 2.22]
+    floats_sum = sum_list(floats)
+    print(floats_sum == sum(floats))
+    print(sum_list.__annotations__)
+print("sum_list(floats) returns {} which is a {}".format(floats_sum, type(floats_sum)))
