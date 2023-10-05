1d4712b7151cb24faa4674fc4bc0d6f41615a423
sentense1 <3xsaint@gmail.com>
2023-10-05 15:09:56 +0100

    task 12

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ Module for type hint """
-from typing import Sequence, Any, Union
+from typing import Sequence, Any, Union, Optional
 
 
-def safe_first_element(lst: Sequence) -> Union[Any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """ Duck typing - first element of a sequence """
     if lst:
         return lst[0]
---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Module for type hint """
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
+    """  Validate the code using mypy. """
+    zoomed_in: List[int] = [
+        item for item in lst
+        for _ in range(factor)
+    ]
+    return zoomed_in
+
+array = (12, 72, 91)  # Use a tuple instead of a list
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)  # Pass an integer as the factor
