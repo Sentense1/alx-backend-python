bd5ee0d1bb2d2bd392b7f9877911c4f4b761c5e7
sentense1 <3xsaint@gmail.com>
2023-10-05 14:05:27 +0100

    task 6 and 7

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -6,11 +6,3 @@ from typing import List, Union
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ TAkes a list of int and float,sums and returns a float """
     return sum(mxd_lst)
-
-
-if __name__ == '__main__':
-    print(sum_mixed_list.__annotations__)
-    mixed = [5, 4, 3.14, 666, 0.99]
-    ans = sum_mixed_list(mixed)
-    print(ans == sum(mixed))
-    print("sum_mixed_list(mixed) returns {} which is a {}".format(ans, type(ans)))
--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,8 +6,3 @@ def to_kv(k: str, v: int | float) -> Tuple[Union[str, float]]:
     """ Taske a str and float/int and returns a tup,le """
     my_tuple: tuple = (k, float(v * 2))
     return my_tuple
-
-if __name__ == '__main__':
-    print(to_kv.__annotations__)
-    print(to_kv("eggs", 3))
-    print(to_kv("school", 0.02))
