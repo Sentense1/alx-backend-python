75674f4eb646bc77fdd46f34e43c29395a7430ff
sentense1 <3xsaint@gmail.com>
2023-10-09 14:59:49 +0100

    modified for pycodestyle

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -3,14 +3,12 @@
 
 
 import asyncio
-from asyncio import create_task
-from typing import Callable, Optional
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> asyncio.Task[float]:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """
     Create an asyncio.Task that generates a random delay.
 
@@ -20,5 +18,5 @@ def task_wait_random(max_delay: int) -> asyncio.Task[float]:
     Returns:
         asyncio.Task[float]: An asyncio.Task representing the asynchronous operation.
     """
-    task = create_task(wait_random(max_delay))
+    task = asyncio.create_task(wait_random(max_delay))
     return task
