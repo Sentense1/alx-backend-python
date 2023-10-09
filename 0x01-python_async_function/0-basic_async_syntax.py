d44a27214ba2124b8116451d2d4a7d6338642d48
sentense1 <3xsaint@gmail.com>
2023-10-09 12:36:35 +0100

    task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-""" This module provides an asynchronous coroutine for generating a random delay. """
-
+""" Provides an asynchronous coroutine for generating a random delay. """
 import asyncio
 import random
 from typing import Optional
@@ -8,10 +7,10 @@ from typing import Optional
 
 async def wait_random(max_delay: int = 10) -> float:
     """ 
-    Generates a random delay between 0 and max_delay (inclusive) seconds and returns it.
+    Generates a random delay between 0 and max_delay and returns it.
     
     Args:
-        max_delay (float, optional): The maximum delay in seconds. Defaults to 10.0.
+        max_delay (float): The maximum delay in seconds. Defaults to 10.
     
     Returns:
         float: The random delay in seconds.
