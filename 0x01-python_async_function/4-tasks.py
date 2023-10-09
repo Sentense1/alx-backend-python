3e97dc2e1d196303db25a8ddeab4511e6e67d891
sentense1 <3xsaint@gmail.com>
2023-10-09 15:09:57 +0100

    created a test dir

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""Module for asynchronous delay operations."""
+""" Module for asynchronous delay operations. """
 
 
 import asyncio
 from typing import List
 
 
-from .3-tasks import task_wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
