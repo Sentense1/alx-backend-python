c302349036b740ed1bc36047b13495fbd0a3e910
sentense1 <3xsaint@gmail.com>
2023-10-09 13:09:58 +0100

    task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
-
-
 """ Module for asynchronous delay operations. """
-
 
 import asyncio
 from typing import Optional, List
-from .0-basic_async_syntax import wait_random
+
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
@@ -19,10 +17,20 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     Returns:
         List[float]: A list of random delays sorted in ascending order.
     """
-    list_delay = [wait_random(max_delay) for times in range(n)]
-    sorted_list = sorted(list_delay)
-    return sorted_list
+    # Create a list of tasks to execute concurrently.
+    # Using list comprehension, create tasks that call the 'wait_random' coroutine
+    # Each task represents an asynchronous delay operation.
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
 
+    # Wait for tasks to complete using asyncio.as_completed(),
+    #   which returns tasks in the order they complete.
+    my_list = [await task for task in asyncio.as_completed(tasks)]
+
+    # Sort the list of delays in ascending order.
+    sorted_list = sorted(my_list)
+
+    # Return the sorted list.
+    return sorted_list
 
 if __name__ == '__main__':
     print(asyncio.run(wait_n(5, 5)))
