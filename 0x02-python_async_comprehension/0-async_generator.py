b90c8cd256fb938b875e27b52bedd5ed2072c92a
sentense1 <3xsaint@gmail.com>
2023-10-10 12:08:50 +0100

    all tasks

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" asynchronous generator that yields random integers with a 1-second delay.
+"""
+
+import asyncio
+import random
+
+
+async def async_generator() -> int:
+    """
+    Asynchronous generator that yields random integers with a 1-second delay.
+
+    Yields:
+        int: Random integers generated between 1 and 100.
+    """
+    for i in range(11):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
+
+if __name__ == '__main__':
+    async def print_yielded_values():
+        """
+        Coroutine that collects values from async_generator and prints them.
+        """
+        result = []
+        async for i in async_generator():
+            result.append(i)
+        print(result)
+
+    asyncio.run(print_yielded_values())
---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" Module for measuring the runtime of an asynchronous operation. """
+
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Measure the runtime of running async_comprehension multiple times.
+
+    Returns:
+        float: Elapsed time in seconds.
+    """
+    start_time = time.perf_counter()
+
+    task_1 = asyncio.create_task(async_comprehension())
+    task_2 = asyncio.create_task(async_comprehension())
+    task_3 = asyncio.create_task(async_comprehension())
+    task_4 = asyncio.create_task(async_comprehension())
+
+    await asyncio.gather(task_1, task_2, task_3, task_4)
+
+    end_time = time.perf_counter()
+
+    elapsed_time = end_time - start_time
+
+    return elapsed_time
+
+
+if __name__ == '__main__':
+    async def main():
+        """
+        Main coroutine that measures and prints the total elapsed time.
+        """
+        return await(measure_runtime())
+
+    print(asyncio.run(main()))
