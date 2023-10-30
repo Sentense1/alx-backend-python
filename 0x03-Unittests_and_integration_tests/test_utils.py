853958b4c4c800ca1803e822bd6366cf2a88fbf4
sentense1 <3xsaint@gmail.com>
2023-10-30 13:12:52 +0000

    corrected the syntax

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -11,17 +11,15 @@ class TestAccessNestedMap(unittest.TestCase):
     """
     Class for unittest case
     """
-    @parameterized.expand({
+    @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
-    })
+    ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """
-        Method to test nexted map function
-        """
+        """ Method to test nexted map function. """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     unittest.main()
