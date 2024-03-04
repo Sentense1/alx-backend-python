0d16ca84215a7413c3324d2d76d09282e019994a
Johnson Oragui <3xsaint@gmail.com>
2024-03-04 07:23:18 +0100

    Update 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
-# Taske a str and float/int and returns a tuple
-""" Module for defining and working with type hints """
+"""
+Module for defining and working with type hints
+"""
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Takes a string k and an int or float v, and returns a tuple."""
+    """
+    Takes a string k and an int or float v, and returns a tuple.
+    """
     return k, float(v * v)
