9d0c6e9765aa9ee9729d33d421c518c3e386e441
Johnson Oragui <3xsaint@gmail.com>
2024-03-04 07:24:34 +0100

    Update 8-make_multiplier.py

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -2,8 +2,11 @@
 """ Module for type hint """
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """Takes a float multiplier and returns a function to multiply a float."""
+    """
+    Takes a float multiplier and returns a function to multiply a float.
+    """
     def multiplier_function(x: float) -> float:
         return x * multiplier
     return multiplier_function
