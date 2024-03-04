c34a707383208a88616d00cc55933998866b5e6b
Johnson Oragui <3xsaint@gmail.com>
2024-03-04 07:19:35 +0100

    Update 1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-
 """ Annotated function that concats. two strings """
 
 
 def concat(str1: str, str2: str) -> str:
-    """ function concat that takes two strings and returns a concatenated string """
+    """
+    function concat that takes two
+    strings and returns a concatenated string
+    """
     return '{}{}'.format(str1, str2)
